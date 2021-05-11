51e00ac232304cfca251360571547444645973cb
mussab123 <mussabassefa8@gmail.com>
2021-05-10 23:56:10 -0600

    git

--- app.js
+++ app.js
@@ -185,8 +185,8 @@ app.post("/login", function (req, res) {
 });
 
 let port = process.env.PORT;
-if(port == null || port == ""){
-    port = 3001;
+if (port == null || port == "") {
+  port = 3000;
 }
 app.listen(port, function () {
   console.log("Server started on port 3000.");
